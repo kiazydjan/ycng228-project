eb7bff32f945986367e3b9017fec77f5d482411d
kiazydjan <karen.iazydjan@gmail.com>
2022-10-02 21:44:56 -0400

    added scripts to concatenate ticker data and data exploration notebook

---
+++ Scripts/combine-csv.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import pandas as pd
+
+os.chdir('C:/Users/karen/PycharmProjects/ycng228-project/.data')
+extension = 'csv'
+filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+combined_pd = pd.concat([pd.read_csv(f) for f in filenames])
+
+combined_pd.to_csv("SP500_data_all.csv" , index=False , encoding='utf-8-sig')
+
+
