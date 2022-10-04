b0d0a29aa8fee23f13598907b41ecda5c8f6983a
kiazydjan <karen.iazydjan@gmail.com>
2022-10-03 20:49:13 -0400

    fixed typo in file name for get ticker info script

--- Scripts/get_ticker_info.py
+++ Scripts/get_ticker_info.py
@@ -1,4 +1,4 @@
 from yahoo_fin import stock_info as si
 
 ticker_info = si.tickers_sp500(True)
-ticker_info.to_csv("_SP500_ticket_info.csv" , index=False , encoding='utf-8-sig')
+ticker_info.to_csv("_SP500_ticker_info.csv" , index=False , encoding='utf-8-sig')
